cc35f6ae182f002e9afa1276c71efdecc4542917
Matilde Morrone <contact@morrone.dev>
2025-07-09 21:39:40 +0200

    Configure node server

--- astro.config.ts
+++ astro.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'astro/config';
 import solidJs from '@astrojs/solid-js';
 
+import node from '@astrojs/node';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [solidJs({ devtools: true })],
+  output: 'server',
+  adapter: node({
+    mode: 'standalone',
+  }),
 });
